19d5369f10c3a092fbadcc88fc2db5c70df60368
Anais <zulfeqan@sheridancollege.ca>
2023-04-08 12:16:40 -0400

    Initial Commit

---
+++ Movie_Picker/Movie_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_PickerApp.swift
+//  Movie_Picker
+//
+//  Created by Anais Zulfequar on 2023-04-08.
+//
+
+import SwiftUI
+
+@main
+struct Movie_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
