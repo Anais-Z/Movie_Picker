31ab05e8e56839bd157cf1c9e0d193a0989addce
Anais <zulfeqan@sheridancollege.ca>
2023-04-08 22:17:23 -0400

    Iterated through the movie list to display each item.

--- Movie_Picker/MovieView.swift
+++ Movie_Picker/MovieView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct MovieView: View {
+    var movies = [Movie]()
+    
     var body: some View {
         Text("Movie View ")
+        
+        //Looping through the movie list
+        List(movies, id:\.id){ movie in
+            VStack{
+                Text("Movie : \(movie.name)")
+                Text("Genre : \(movie.genre)")
+                Text("Rating : \(movie.rating) stars")
+            }
+            
+        }
+        
     }
 }
 
